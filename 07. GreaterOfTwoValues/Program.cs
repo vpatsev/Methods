6e569c9c09ddf7a352ede3dd778a0089e046a5c9
Ventsislav Patsev <164578745+vpatsev@users.noreply.github.com>
2025-02-03 06:00:06 +0200

    Add files via upload

---
+++ 07. GreaterOfTwoValues/Program.cs
@@ -0,0 +1,42 @@
+﻿string type = Console.ReadLine();
+
+GetGreaterOfTwoValues(type);
+static void GetGreaterOfTwoValues(string type) //тук може и с static string ...
+{
+
+    switch (type)
+    {
+        case "int":
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                Console.WriteLine(num1);
+            }
+            else { Console.WriteLine(num2); }
+            break;
+
+        case "string":
+            string str1 = Console.ReadLine();
+            string str2 = Console.ReadLine();
+            if (string.Compare(str1, str2) > 0)
+            {
+                Console.WriteLine(str1);
+            }
+            else
+            {
+                Console.WriteLine(str2);
+            }
+            break;
+        case "char":
+            char char1 = char.Parse(Console.ReadLine());
+            char char2 = char.Parse(Console.ReadLine());
+
+            if (char1 < char2) { Console.WriteLine(char2); }
+            else { Console.WriteLine(char1); }
+            break;
+
+    } //return type;
+}
+
