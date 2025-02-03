2fe58d02265e194d4d69d1e4b1898471c54a3110
Ventsislav Patsev <164578745+vpatsev@users.noreply.github.com>
2025-02-03 05:58:36 +0200

    Add files via upload

---
+++ 01.SignofIntegerNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿int number = int.Parse(Console.ReadLine()); 
+ ReadsIntegerNumber(number);
+static void ReadsIntegerNumber(int number)
+{
+    if (number > 0)
+    {
+        Console.WriteLine($"The number {number} is positive.");
+    }
+    else if (number < 0)
+    {
+        Console.WriteLine($"The number {number} is negative.");
+    }
+    else
+    {
+        Console.WriteLine($"The number {number} is zero.");
+    }
+}
---
+++ 03.PrintingTriangle/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+int num = int.Parse(Console.ReadLine());
+
+
+
+PrintSecondPartOfTriangle(num);
+
+
+static void PrintSecondPartOfTriangle(int num)
+{
+
+    for (int i = 1; i < num; i++)
+    {
+        PrintFirstPartOfTriange(1, i);
+    }
+    for (int i = num; i >= 1; i--)
+    {
+        PrintFirstPartOfTriange(1, i);
+
+    }
+
+}
+static void PrintFirstPartOfTriange(int start, int end)
+{
+
+    for (int i = start; i <= end; i++)
+    {
+        Console.Write($"{i} ");
+    }
+    Console.WriteLine();
+
+}
